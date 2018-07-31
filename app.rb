ea506acd59b6a84118e80946e2f1b8f6c4ab7049
anumahmad <anumqahmad@gmail.com>
2018-07-31 18:35:28 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do  #homepage- whatever my local text is
+    "Hello World"
+  end 
+  
+  #local host/ name 
+  get '/name' do 
+    "My name is Anum"
+    end 
 end
